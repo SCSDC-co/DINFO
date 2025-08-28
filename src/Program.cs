d9801347201b695f3f21005e3dbd34c2a14ecd4d
Sckab <giulianodeamicis2009@gmail.com>
2025-08-28 10:05:46 +0200

    refactor: make Program.cs more readable

--- src/Program.cs
+++ src/Program.cs
@@ -9,36 +9,33 @@ public static class Program
     public static void Main(string[] args)
     {
         if (args.Length == 0)
-        {
             DirectoryHelper.ProcessDirectory(Directory.GetCurrentDirectory());
-        }
-        else
+
+        foreach (string arg in args)
         {
-            foreach (string arg in args)
+            if (!File.Exists(arg) && !Directory.Exists(arg))
+                Console.WriteLine($"Invalid input, skipping {arg}");
+
+            var attributes = File.GetAttributes(arg);
+
+            if (attributes.HasFlag(FileAttributes.Directory))
             {
-                if (!File.Exists(arg) && !Directory.Exists(arg))
-                    Console.WriteLine($"Invalid input, skipping {arg}");
+                DirectoryHelper.ProcessDirectory(arg);
+            }
+            else if (attributes.HasFlag(FileAttributes.Normal))
+            {
+                Console.WriteLine($"Lines of {arg}: {FilesHelper.CountLines(arg)}");
 
-                var attributes = File.GetAttributes(arg);
-
-                if (attributes.HasFlag(FileAttributes.Directory))
-                {
-                    DirectoryHelper.ProcessDirectory(arg);
-                }
-                else if (attributes.HasFlag(FileAttributes.Normal))
-                {
-                    Console.WriteLine($"Lines of {arg}: {FilesHelper.CountLines(arg)}");
-
-                    GlobalsUtils.filesProcessed++;
-                    GlobalsUtils.totalFiles++;
-                    GlobalsUtils.totalLines += FilesHelper.CountLines(arg);
-                }
-                else
-                {
-                    Console.WriteLine($"Invalid input, skipping {arg}");
-                }
+                GlobalsUtils.filesProcessed++;
+                GlobalsUtils.totalFiles++;
+                GlobalsUtils.totalLines += FilesHelper.CountLines(arg);
+            }
+            else
+            {
+                Console.WriteLine($"Invalid input, skipping {arg}");
             }
         }
+
         Console.WriteLine();
         Console.WriteLine(new string('-', 20));
         Console.WriteLine();
